89640b6edf6eb1c81a770bbef82e81cc58a2980e
Konstantin <kaplan.sk8@mail.ru>
2023-02-02 23:16:42 +0300

    first upload

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import style from "./Navbar.module.scss";
+
+export const Navbar = () => {
+	return (
+		<div className={style.flexbar}>
+			<h4>X</h4>
+			<h4>X</h4>
+			<h4>X</h4>
+			<h4>X</h4>
+		</div>
+	);
+};
